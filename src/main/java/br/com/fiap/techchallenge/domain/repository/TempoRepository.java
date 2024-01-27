0b3a8e30496e0fb3b4a095874ae541edb0803df5
DouglasPFS <douglas_rm@live.com>
2024-01-27 03:01:23 -0300

    commit alterações back

--- src/main/java/br/com/fiap/techchallenge/domain/repository/TempoRepository.java
+++ src/main/java/br/com/fiap/techchallenge/domain/repository/TempoRepository.java
@@ -1,6 +1,5 @@
 package br.com.fiap.techchallenge.domain.repository;
 
-import br.com.fiap.techchallenge.domain.model.Condutor;
 import br.com.fiap.techchallenge.domain.model.Tempo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
